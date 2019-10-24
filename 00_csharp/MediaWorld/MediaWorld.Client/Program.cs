ae28c9183884348e96a64a0510fc51824251c07a
tyler gilbert <tylerdavidgilbert@gmail.com>
2019-10-24 17:48:08 -0500

    add MediaWorld

---
+++ 00_csharp/MediaWorld/MediaWorld.Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MediaWorld.Domain.Models;
+
+namespace MediaWorld.Client
+{
+    /// <summary>
+    /// contains the start point
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// starts the application
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            var helper = MediaSingleton.GetInstance();
+            // var helper2 = new MediaSingleton();
+            Console.WriteLine(helper);
+        }
+    }
+}
