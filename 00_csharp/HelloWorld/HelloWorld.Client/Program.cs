966facd3a5acb70d59f2b79f45a9d3b723fccb46
tyler gilbert <tylerdavidgilbert@gmail.com>
2019-10-24 15:33:21 -0500

    add HelloWorld

---
+++ 00_csharp/HelloWorld/HelloWorld.Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using sc = System.Console; // import with an alias
+
+namespace HelloWorld.Client
+{
+    /// <summary>
+    /// This is an XML comment. You make it by pressing /// 
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// starts the program
+        /// </summary>
+        private static void Main()
+        {
+            sc.WriteLine("Hello World!");
+        }
+    }
+}
