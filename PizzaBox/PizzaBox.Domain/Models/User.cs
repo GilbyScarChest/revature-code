ef5a081be2f84f322b925f0e3b8a8a11538ba6de
tyler gilbert <tylerdavidgilbert@gmail.com>
2019-10-28 18:49:18 -0500

    add dependancies and classes

---
+++ PizzaBox/PizzaBox.Domain/Models/User.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace PizzaBox.Domain.Models
+{
+    public class User
+    {
+        
+    }
+}
---
+++ PizzaBox/PizzaBox.Domain/Singletons/PizzaBoxSingleton.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace PizzaBox.Domain.Singletons
+{
+    public class PizzaBoxSingleton
+    {
+        
+    }
+}
